7b1652974e4e0a66bc692be61f15b5941cc04e00
vic8tor <timongrinni@gmail.com>
2022-01-05 20:47:53 +0300

    Added data transfer

--- MoviesTime/ViewControllers/TabBarViewController.swift
+++ MoviesTime/ViewControllers/TabBarViewController.swift
@@ -10,21 +10,18 @@ import UIKit
 class TabBarViewController: UITabBarController {
 
     // MARK: - Public Properties
-    let Movies = Movie.getMovie()
+    let movie: [Movie] = []
     
     // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        selectVC()
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Private methods
+    private func selectVC() {
+        guard let movieListVC = viewControllers?.first as? MovieListViewController else { return }
+        movieListVC.movie = movie
     }
-    */
-
+    
+    
 }
