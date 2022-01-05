b732e5e528e7136325052ae7c57526d0f9087a12
vic8tor <timongrinni@gmail.com>
2022-01-05 13:49:20 +0300

    Set structure projects

---
+++ MoviesTime/Services/DataManager.swift
@@ -0,0 +1,15 @@
+//
+//  DataManager.swift
+//  MoviesTime
+//
+//  Created by Victor on 05.01.2022.
+//
+
+import Foundation
+
+class DataManager {
+    static let shared =  DataManager()
+        let names = ["Matrix"]
+    
+    private init() {}
+}
---
+++ MoviesTime/Services/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  MoviesTime
+//
+//  Created by Victor on 05.01.2022.
+//
+
+import Foundation
+
+struct Movie {
+    let name: String
+    let genre: String
+    let image: String
+    let raiting: Float
+    
+}
