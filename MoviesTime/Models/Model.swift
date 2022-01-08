4f0503a61367bd39ffcfc195199e37eef95d45a5
vic8tor <timongrinni@gmail.com>
2022-01-08 13:20:14 +0300

    Change image and model

--- MoviesTime/Models/Model.swift
+++ MoviesTime/Models/Model.swift
@@ -9,30 +9,37 @@ import Foundation
 
 struct Movie {
     let name: String
-    let genre: String
-    let year: String
+    let genre: [String]
+    let image: String
+    let desctiption: String
     var raiting: Float {
-        Float.random(in: 1...10)
+        Float.random(in: 7...10)
     }
-    var image: String {
-        "\(name) - \(year)"
+    var imageSmall: String {
+        image + "-small"
     }
+    var imageWide: String {
+        image + "-wide"
+    }
+    
     static func getMovie() -> [Movie] {
         var movies: [Movie] = []
         
         let name = DataManager.shared.names
         let genre = DataManager.shared.genre
-        let year = DataManager.shared.years
+        let image = DataManager.shared.image
+        let description = DataManager.shared.description
         
         let iteration = min(
             name.count,
             genre.count,
-            year.count
+            description.count
         )
     
         for index in 1..<iteration {
-            movies.append(Movie(name: name[index], genre: genre[index], year: year[index]))
+            movies.append(Movie(name: name[index], genre: genre[index], image: image[index]  , desctiption: description[index]))
         }
     return movies
     }
+    
 }
