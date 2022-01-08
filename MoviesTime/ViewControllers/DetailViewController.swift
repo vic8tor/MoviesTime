d3073cfef8c09725599c0a39393fb85171f007d1
vic8tor <timongrinni@gmail.com>
2022-01-08 21:23:39 +0300

    Customized detailViewControlles

--- MoviesTime/ViewControllers/DetailViewController.swift
+++ MoviesTime/ViewControllers/DetailViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var wideImage: UIImageView!
+    @IBOutlet weak var titleMovieLabel: UILabel!
+    @IBOutlet weak var genreMovieLabel: UILabel!
+    @IBOutlet weak var raitingMovieLabel: UILabel!
+    @IBOutlet weak var summaryTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
