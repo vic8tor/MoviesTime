ecb020f6ab1aa21c727843c526929a37733c0fa2
vic8tor <timongrinni@gmail.com>
2022-01-14 19:12:40 +0300

    Added method for download image and set values

--- MoviesTime/ViewControllers/CustomTableViewCell.swift
+++ MoviesTime/ViewControllers/CustomTableViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 class CustomTableViewCell: UITableViewCell {
     // MARK: - @IBOutlets
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var yearLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
     @IBOutlet weak var raitingLabel: UILabel!
     @IBOutlet weak var imageOfMovies: UIImageView!
@@ -25,10 +25,25 @@ class CustomTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-    
-        
     }
     // MARK: - Public Methods
+    func configure(with movie: Movie) {
+        titleLabel.text = movie.title
+        yearLabel.text = movie.year
+        summaryLabel.text = movie.overview
+        raitingLabel.text = String(format: "%.1f", Double(movie.rate ?? 0))
+        
+        DispatchQueue.main.async {
+            guard let image = movie.image else { return }
+            let urlString = Url.imageUrl.rawValue + image
+            
+            guard let url = URL(string: urlString) else { return }
+            guard let imageData = try? Data(contentsOf: url) else { return }
+            
+            self.imageOfMovies.image = UIImage(data: imageData)
+        }
+//        self.imageOfMovies.image = nil
+    }
     
     // MARK: - Private Methods
 
