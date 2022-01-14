1d77d124a6cfe168c7dfcb8a2c2893030d8a2eb3
vic8tor <timongrinni@gmail.com>
2022-01-14 18:11:14 +0300

    Fix model for Json structure

---
+++ MoviesTime/Models/MoviesModel.swift
@@ -0,0 +1,39 @@
+//
+//  Model.swift
+//  MoviesTime
+//
+//  Created by Victor on 05.01.2022.
+//
+
+import Foundation
+
+struct MoviesData: Decodable {
+    let movies: [Movie]
+    
+    private enum CodingKeys: String, CodingKey {
+        case movies = "results"
+    }
+    
+}
+struct Movie: Decodable {
+    let title: String?
+//    let genre: String?
+    let year: String?
+    let image: String?
+//    let wideImage: String?
+    let overview: String?
+    var rate: Double?
+    
+    private enum CodingKeys: String, CodingKey {
+        case title, overview
+        case year = "release_date"
+        case rate = "vote_average"
+        case image = "poster_path"
+    }
+}
+
+enum Url: String {
+    case moviesTB = "https://api.themoviedb.org/3/movie/top_rated?api_key=97fd25ac93042d4d2d469b66b1505de7"
+}
+
+
